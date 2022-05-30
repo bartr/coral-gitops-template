0c84ca1d89993bd336662eedd8342b735a753116
bartr <bartr@microsoft.com>
2022-05-30 02:31:34 +0000

    updated flt create

--- vm/setup/flux-setup.sh
+++ vm/setup/flux-setup.sh
@@ -33,15 +33,63 @@ then
   exit 1
 fi
 
-git pull
-kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/namespace.yaml"
-flux create secret git flux-system -n flux-system --url https://github.com/retaildevcrews/bartr-fleet -u gitops -p "$AKDC_PAT"
-kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/controllers.yaml"
-sleep 3
-kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/source.yaml"
-sleep 2
-kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/*.yaml"
-sleep 5
+status_code=1
+retry_count=0
+
+until [ $status_code == 0 ]; do
+
+  echo "flux retries: $retry_count"
+  echo "$(date +'%Y-%m-%d %H:%M:%S')  flux retries: $retry_count" >> "$HOME/status"
+
+  if [ $retry_count -gt 0 ]
+  then
+    sleep $((RANDOM % 30+15))
+  fi
+
+  retry_count=$((retry_count + 1))
+
+  flux bootstrap git \
+  --url "https://github.com/$AKDC_REPO" \
+  --branch "$AKDC_BRANCH" \
+  --password "$(cat /home/akdc/.ssh/akdc.pat)" \
+  --token-auth true \
+  --path "./clusters/$AKDC_CLUSTER"
+
+  status_code=$?
+done
+
+echo "adding flux sources"
+echo "$(date +'%Y-%m-%d %H:%M:%S')  adding flux sources" >> "$HOME/status"
+
+flux create source git gitops \
+--namespace flux-system \
+--url "https://github.com/$AKDC_REPO" \
+--branch "$AKDC_BRANCH" \
+--secret-ref flux-system
+
+flux create kustomization bootstrap \
+--namespace flux-system \
+--source GitRepository/gitops \
+--path "./deploy/bootstrap/$AKDC_CLUSTER" \
+--prune true \
+--interval 1m
+
+flux create kustomization apps \
+--namespace flux-system \
+--source GitRepository/gitops \
+--path "./deploy/apps/$AKDC_CLUSTER" \
+--prune true \
+--interval 1m
+
+# git pull
+# kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/namespace.yaml"
+# flux create secret git flux-system -n flux-system --url https://github.com/retaildevcrews/bartr-fleet -u gitops -p "$AKDC_PAT"
+# kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/controllers.yaml"
+# sleep 3
+# kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/source.yaml"
+# sleep 2
+# kubectl apply -f "$HOME/gitops/deploy/flux/$AKDC_CLUSTER/flux-system/dev/flux-system/*.yaml"
+# sleep 5
 
 flux reconcile source git gitops
 
